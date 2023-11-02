720521334ebee18e77abf82518ad5c98d922c0ca
Eric Moritsuka <ericthmoritsuka@gmail.com>
2023-11-02 11:56:43 -0300

    adjusted timer buttons

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const players = document.querySelectorAll(".jogador");
+    let timerRunning = false;
+    let timerPaused = false;
+
+    const pauseButton = document.getElementById("pause");
+    pauseButton.disabled = true;
 
     players.forEach(player => {
         const upButton = player.querySelector(".cima");
@@ -8,16 +13,22 @@ document.addEventListener("DOMContentLoaded", function() {
         const teamTotalElement = player.closest(".time").querySelector(".total");
 
         upButton.addEventListener("click", function() {
-            const currentScore = parseInt(scoreElement.textContent, 10);
-            scoreElement.textContent = currentScore + 1;
-            updateTeamTotal(teamTotalElement);
+            if (timerRunning) {
+                const currentScore = parseInt(scoreElement.textContent, 10);
+                if (currentScore < 5){
+                    scoreElement.textContent = currentScore + 1;
+                    updateTeamTotal(teamTotalElement);
+                }
+            }
         });
 
         downButton.addEventListener("click", function() {
-            const currentScore = parseInt(scoreElement.textContent, 10);
-            if (currentScore > 0) {
-                scoreElement.textContent = currentScore - 1;
-                updateTeamTotal(teamTotalElement);
+            if (timerRunning){
+                const currentScore = parseInt(scoreElement.textContent, 10);
+                if (currentScore > 0) {
+                    scoreElement.textContent = currentScore - 1;
+                    updateTeamTotal(teamTotalElement);
+                }
             }
         });
     });
@@ -39,15 +50,20 @@ document.addEventListener("DOMContentLoaded", function() {
     const minutesElement = document.getElementById("minutes");
     const secondsElement = document.getElementById("seconds");
 
-    // Timer buttons
+    // Timer buttons, pause is set in the beginning
     const startButton = document.getElementById("start");
-    const pauseButton = document.getElementById("pause");
     const resetButton = document.getElementById("reset");
 
     // Start button click event
     startButton.addEventListener("click", function() {
         if (!timer) {
+            timerRunning = true;
+            if (timerPaused){
+                startButton.textContent = "Continuar";
+                timerPaused = false;
+            }
             timer = setInterval(updateTimer, 1000);
+            pauseButton.disabled = false;
         }
     });
 
@@ -55,14 +71,21 @@ document.addEventListener("DOMContentLoaded", function() {
     pauseButton.addEventListener("click", function() {
         clearInterval(timer);
         timer = null;
+        timerRunning = false;
+        timerPaused = true;
+        startButton.textContent = "Continuar";
     });
 
     // Reset button click event
     resetButton.addEventListener("click", function() {
-        const confirmReset = window.confirm("Are you sure you want to reset the scores and the timer?");
+        const confirmReset = window.confirm("Você tem certeza que deseja resetar o timer e o placar?");
         if (confirmReset){
             clearInterval(timer);
             timer = null;
+            timerRunning = false;
+            timerPaused = false;
+            startButton.textContent = "Começar"
+            pauseButton.disabled = true;
             minutes = 30;
             seconds = 0;
             updateTimerDisplay();
